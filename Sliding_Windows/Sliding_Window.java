a6bb25354e865e171ba24ecbfc23f3ed93de8ff5
Subhrajit Deka <dekasubhrajit2007@gmail.com>
2025-08-28 09:06:59 +0530

    Commiting second folder

---
+++ Sliding_Windows/Sliding_Window.java
@@ -0,0 +1,13 @@
+package Sliding_Windows;
+
+/**
+ * Utility of Sliding Windows :
+ * 1) Sub array : continuous pieces of array
+ * 2) Substrings
+ * 3) Types : K sized / Variable sized
+ */
+public class Sliding_Window {
+    public static void main(String[] args) {
+
+    }
+}
