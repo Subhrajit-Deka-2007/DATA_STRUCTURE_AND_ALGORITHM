4072a683b54beb97c5c2b4c25e43e0d228d56b25
Subhrajit Deka <dekasubhrajit2007@gmail.com>
2025-10-05 18:26:45 +0530

    Commiting

--- Greedy/MaxSumOfArrayAfterKNegations.java
+++ Greedy/MaxSumOfArrayAfterKNegations.java
@@ -38,6 +38,10 @@ import java.util.PriorityQueue;
  1 <= k <= 104
  */
 public class MaxSumOfArrayAfterKNegations {
+    /**Optimize  Also if the elements in the array all are +ve && if k is even then we need to do anything at that time we just need to do anything
+     * as the array will remain the same */
+
+
     public static void main(String[] args) {
         int [] nums ={-2,5,0,2,-2};
         int k = 3;
@@ -50,52 +54,93 @@ public class MaxSumOfArrayAfterKNegations {
 /**======================================= METHOD 1 : USING PRIORITY QUEUE ===========================================================*/
             /* GREEDY APPROACH EACH TIME WE ARE CHOOSING THE MINIMUM ELEMENT */
             PriorityQueue<Integer> pq = new PriorityQueue<>();
-            for(int i =0;i<nums.length;i++)pq.add(nums[i]);
+            for (int i = 0; i < nums.length; i++) pq.add(nums[i]);
         /*
         lOG 1 + LOG 2 + LOG 3 +--- +LOG N = LOG N!~ N LOG N
         */
             int rmv;
-            for(int i =0;i<k;i++){
+            for (int i = 0; i < k; i++) {
                 rmv = pq.remove();
-                pq.add(-1*rmv);
+                pq.add(-1 * rmv);
                 /* K*(LOG N-1 + LOG N)
                  */
             }
-            int sum =0;
-            for(int ele :pq)sum+=ele;
+            int sum = 0;
+            for (int ele : pq) sum += ele;
 
-            return sum ;
+            return sum;
         /*
         T.C = N LOG N + K LOG N-1 + K LOG N +O(N(FOR TRACVERSING ))
             = O( (N+K) LOG N + K LOG N-1 + N )
         S.C =O(N)
         */
         }
-        public  int largestSumAfterKNegations_2(int[] nums, int k) {
 
-                    Arrays.sort(nums);// N LOG N
-                    int minele;
-                    int minidx;
-                    while(k-->0){
-                        minele =nums[0];
-                        minidx =0;
-                        for(int i =1;i<nums.length;i++){
-                            if(nums[i]<minele){
-                                minele = nums[i];
-                                minidx =i;
-                            }
-                        }
-                        nums[minidx]= -1*minele;
+        public int largestSumAfterKNegations_2(int[] nums, int k) {
+
+            Arrays.sort(nums);// N LOG N
+            int minele;
+            int minidx;
+            while (k-- > 0) {
+                minele = nums[0];
+                minidx = 0;
+                for (int i = 1; i < nums.length; i++) {
+                    if (nums[i] < minele) {
+                        minele = nums[i];
+                        minidx = i;
                     }
-
-                    int maxsum =0;
-                    for(int ele:nums)maxsum+=ele;
-                    return maxsum;
                 }
+                nums[minidx] = -1 * minele;
+            }
+
+            int maxsum = 0;
+            for (int ele : nums) maxsum += ele;
+            return maxsum;
+        }
     /*
     T.C =O(N LOG N {FOR SORTING } + K*N +N )
     S.C =O(1)
     */
+
+        public int largestSumAfterKNegations_3(int[] nums, int k) {
+            /**
+              Optimize  Also if the elements in the array all are +ve && if k is even then we need
+              to do anything at that time we just need to do anything
+              as the array will remain the same
+             */
+
+            Arrays.sort(nums);// N LOG N
+            int minele;
+            int minidx;
+            int pos = 0;
+            while (k > 0) {
+                pos = 0;
+                minele = nums[0];
+                minidx = 0;
+                if (nums[0] > 0) pos++;
+                for (int i = 1; i < nums.length; i++) {
+                    if (nums[i] > 0) pos++;
+                    if (nums[i] < minele) {
+                        minele = nums[i];
+                        minidx = i;
+                    }
+
+                }
+                if (pos == nums.length && k % 2 == 0) break;
+                /*
+                 ALSO HANDLE CASES WHEN THE GIVEN ARRAY IS ALREADY POSITIVE AND K IS EVEN ALSO LIKE AFTER CONVERTING AN ARRAY
+                 DURING THE OPERATION THE ARRAY ELEMENT BECOME +VE AND K IS EVEN THEN NO NEED TO CHECK FURTHER JUST BREAK IT
+                 OPTIMIZE
+                 */
+
+                k--;
+                nums[minidx] = -1 * minele;
             }
+
+            int maxsum = 0;
+            for (int ele : nums) maxsum += ele;
+            return maxsum;
+        }
+    }
 
 
